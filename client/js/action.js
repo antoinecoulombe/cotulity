33469cb84027148971d42b4808378073ef2a448d
Unknown <alexgoodsun@hotmail.com>
2018-11-11 18:00:02 -0500

    Trying to fix weird css on svg add
    still not fixed :'(

--- client/js/action.js
+++ client/js/action.js
@@ -1,7 +1,7 @@
-var jsdom = require(jsdom);
-const {JSDOM} = jsdom;
-const dom = new JSDOM(html);
-const $ = (require('jquery'))(dom.window);
-$("login_action").click((e) =>{
-    alert("allo");
+$(function(){
+    $('#login_action').click((e) =>{
+        $('#login_load').show();
+        $('#login_action').hide();
+
+    });
 });
