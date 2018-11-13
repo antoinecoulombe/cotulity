05cb68a52f618ee8ea45bf4306e7ddac7de888f4
Antoine <coulombe.antoine@hotmail.com>
2018-11-13 01:32:41 -0500

    Styled and animated connection page

--- client/js/action.js
+++ client/js/action.js
@@ -1,7 +1,23 @@
-$(function(){
-    $('#login_action').click((e) =>{
-        $('#login_load').show();
-        $('#login_action').hide();
+$(function() {
+    showLoginForm();
 
+    $('#submit-go').click((e) => {
+        $('#submit-go').fadeOut(200, function() {
+            $('#submit-go').hide();
+            $('#submit-load').css("display", "inline-block");
+        });
     });
 });
+
+function showLoginForm() {
+    $("#logo").animate({ opacity: 1 }, 750);
+
+    setTimeout(function() {
+        $("#logo").animate({ marginBottom: 35 }, 400);
+        $("#container").animate({ width: 487 }, 400);
+
+        setTimeout(function() {
+            $("#login").animate({ opacity: 1 }, 300);
+        }, 500);
+    }, 1250);
+}
