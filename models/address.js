c6a4d32cddbeb8e24e41bd7c4d819ab79ff051d4
Unknown <alexgoodsun@hotmail.com>
2018-11-24 14:38:13 -0500

    Added npm phone-formmater, finished model adress.js

--- models/address.js
+++ models/address.js
@@ -1,29 +1,65 @@
+const phoneFormatter = require('phone-formatter');
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Address = sequelize.define('Address', {
     number: {
       type: DataTypes.INTEGER,
-      validate: {}
+      validate: {
+        isNumeric: true,
+        isNotNull: false,
+        len: [10, 15]
+      },
+      get() {
+        return this.getDataValue('number');
+      },
+      set(val) {
+        let regionnalCode = val.lenght() - 10;
+        let n = "+";
+        for (let i = 0; i < regionnalCode; ++i)
+          n += "N";
+        n += "(NNN) NNN-NNNN";
+        this.setDataValue('number', phoneFormatter.format(val, n));
+      },
     },
     street: {
       type: DataTypes.STRING,
-      validate: {}
+      validate: {
+        isNotNull: false
+      }
     },
     city: {
       type: DataTypes.STRING,
-      validate: {} 
+      validate: {
+        isNotNull: true,
+        notEmpty: true
+      }
     },
     zipcode: {
       type: DataTypes.STRING(30),
-      validate: {} 
+      validate: {
+        isNotNull: false
+      },
+      get() {
+        return this.getDataValue('zipcode');
+
+      },
+      set(val) {
+        this.setDataValue('zipcode', val.toUpperCase());
+      }
     },
     state: {
       type: DataTypes.STRING,
-      validate: {} 
+      validate: {
+        isNotNull: true,
+        notEmpty: true
+      }
     },
     country: {
       type: DataTypes.STRING,
-      validate: {} 
+      validate: {
+        isNotNull: true,
+        notEmpty: true
+      }
     }
   }, {
     timestamps: true,
@@ -32,8 +68,11 @@ module.exports = (sequelize, DataTypes) => {
     freezeTableName: false,
     tableName: 'addresses'
   });
-  Address.associate = function(models) {
-    Address.belongsTo(models.AddressType, {foreignKey: 'addressTypeId', sourceKey: 'id'});
+  Address.associate = function (models) {
+    Address.belongsTo(models.AddressType, {
+      foreignKey: 'addressTypeId',
+      sourceKey: 'id'
+    });
   };
   return Address;
 };
