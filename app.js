1894c56a98523126a86faed802659d6f68118723
Antoine <coulombe.antoine@hotmail.com>
2018-11-26 01:03:03 -0500

    Modified how less/css styles are handled + reorganized folders

--- app.js
+++ app.js
@@ -7,21 +7,17 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../..', '/client')));
+app.use(express.static(path.join(__dirname, '/client')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve('server/html/index.html'));
+    res.sendFile(path.resolve('views/index.html'));
 });
 
 app.post('/', (req, res) => {
-    res.sendFile(path.resolve('server/html/apps.html'));
-});
-
-app.get('/tiles', (req, res) => {
-    res.sendFile(path.resolve('server/html/tiles.html'));
+    res.sendFile(path.resolve('views/apps.html'));
 });
 
 app.listen(3000);
 
 console.log('Starting server on port 3000');
-console.log(path.join(__dirname, '../..', '/client'));
+console.log(path.join(__dirname, '/client'));
