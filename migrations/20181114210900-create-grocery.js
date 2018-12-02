4c8f4846c072253b8d119a9e95aa7cc72fd7d7c9
Antoine <coulombe.antoine@hotmail.com>
2018-12-02 17:24:26 -0500

    Added grocery table and model

--- migrations/20181114210900-create-grocery.js
+++ migrations/20181114210900-create-grocery.js
@@ -1,19 +1,34 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('app_categories', {
+    return queryInterface.createTable('groceries', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      paidBillId: { // Multiple groceries can be selected together to create a bill which will have a bill-item for each grocery selected
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        references: {
+          model: 'paid_bills',
+          key: 'id'
+        }
+      },
       name: {
         allowNull: false,
         type: Sequelize.STRING
       },
-      description: {
+      notes: {
         type: Sequelize.TEXT
+      },
+      soldAt: {
+        type: Sequelize.STRING
+      },
+      approximativeCost: {
+        type: Sequelize.DECIMAL(19, 4)
       },
       createdAt: {
         allowNull: false,
@@ -32,6 +47,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('app_categories');
+    return queryInterface.dropTable('groceries');
   }
 };
---
+++ models/grocery.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Grocery = sequelize.define('Grocery', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validation: {
+        notEmpty: true
+      }
+    },
+    notes: {
+      type: DataTypes.TEXT
+    },
+    soldAt: {
+      type: DataTypes.STRING
+    },
+    approximativeCost: {
+      type: DataTypes.DECIMAL(19, 4)
+    }
+  }, {
+    timestamps: true,
+    paranoid: true,
+    underscored: false,
+    freezeTableName: false,
+    tableName: 'groceries'
+  });
+  Grocery.associate = function(models) {
+    Grocery.belongsTo(models.PaidBill, {
+      foreignKey: 'paidBillId',
+      sourceKey: 'id'
+    });
+  };
+  return Grocery;
+};
