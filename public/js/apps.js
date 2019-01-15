a6bdde8d84b4afb474adfbf789c06eb3983f458d
Antoine <coulombe.antoine@hotmail.com>
2019-01-15 11:18:20 -0500

    Fixed app placement in apps.ejs + continued design

--- public/js/apps.js
+++ public/js/apps.js
@@ -1,17 +1,40 @@
-$(function() {
+
+var previousWidth = $(window).width(),
+    previousHeight = $(window).height();
+
+$(() => {
     setAppContainer($('.app').length);
+
+    $(window).resize(() => {
+        if (hasChanged(previousWidth, $(window).width()) || hasChanged(previousHeight, $(window).height()))
+            setAppContainer($('.app').length);
+
+        previousWidth = $(window).width();
+        previousHeight = $(window).height();
+    });
 });
 
-function setAppContainer(appCount) {
-    // TODO: handle apps < boxPerRow
+function hasChanged(previous, current) {
+    return previous != current; // && current % 10 == 0;
+}
 
-    let windowWidth = $(window).width();
-    let boxPerRow = Math.floor(windowWidth * 0.48 / $('.app').outerWidth(true));
-    let containerWidth = boxPerRow * $('.app').outerWidth(true);
-    let containerHeight = Math.ceil(appCount/boxPerRow) * $('.app').outerHeight(true);
+function setAppContainer(appCount) {
+    let windowWidth = $(window).width(),
+        boxPerRow = Math.floor(windowWidth * 0.48 / $('.app').outerWidth(true)),
+        containerWidth = (boxPerRow < 2 ? 2 : boxPerRow) * $('.app').outerWidth(true),
+        containerHeight = Math.ceil(appCount/boxPerRow) * $('.app').outerHeight(true),
+        overflow = 'visible',
+        maxContainerHeight = $(window).height() - $('#logo').outerHeight(true) * 2;
+
+    if (containerHeight > maxContainerHeight) {
+        containerHeight = Math.floor(maxContainerHeight / $('.app').outerHeight(true));
+        containerHeight = (containerHeight < 2 ? 2 : containerHeight) * $('.app').outerHeight(true);
+        overflow = 'overlay';
+    }
 
     $('#apps').css({
         width: containerWidth,
-        height: containerHeight
+        height: containerHeight,
+        overflowY: overflow
     });
 }
