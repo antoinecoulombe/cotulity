2e88ae8efe94370eeef7ee7503bfed04f569b38b
Antoine <coulombe.antoine@hotmail.com>
2021-08-03 23:59:50 -0400

    Fixed docker/localhost database connection

---
+++ api/db/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    username: 'cot_dev',
+    password: 'dev_pwd',
+    database: 'cotulity_dev',
+    host: process.env.DB_HOST, // docker = 'db', local = '127.0.0.1'
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: { autoJsonmap: false },
+  },
+  test: {
+    username: 'cot_test',
+    password: 'test_pwd',
+    database: 'cotulity_test',
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: { autoJsonmap: false },
+  },
+  production: {
+    username: 'cot_prod',
+    password: 'prod_pwd',
+    database: 'cotulity',
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: { autoJsonmap: false },
+  },
+};
