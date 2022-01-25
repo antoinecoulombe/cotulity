3c09f5a8b6b50663dd294f72906739efb2cc6873
Antoine <coulombe.antoine@hotmail.com>
2022-01-25 00:52:35 -0500

    Implemented server side typescript debugging in vs code

--- api/dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const app = require('./app.js');
-require('./routes/_utils/CronJobs');
-// Express Start
-app.listen(app.get('port'), () => {
-    return console.log(`server is listening on ${app.get('port')}`);
-});
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,4 +1,4 @@
-const app = require('./app.js');
+const app = require('./app.ts');
 require('./routes/_utils/CronJobs');
 
 // Express Start
